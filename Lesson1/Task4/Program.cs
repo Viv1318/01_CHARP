152a425ae23b1972ba0da252617471c1880562ed
oleg voronin <oleg83voronin@gmail.com>
2024-05-24 12:36:49 +0300

    add Task4:lesson1

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 11;
+int e = 4;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
