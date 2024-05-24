0e2ab2161186709c9561f542d41da6dfb53fee78
oleg voronin <oleg83voronin@gmail.com>
2024-05-24 14:47:28 +0300

    add task3:lesson2

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// int n = 10;
+int[] array = { 2,6,98,12,45,3,8,25,67,15};
+int i = 0;
+while (i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
