1cac7f087424b70f11673cc332587b1d64f68d7b
oleg voronin <oleg83voronin@gmail.com>
2024-05-25 16:25:59 +0300

    add Task1:lesson3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double ColculateFormuls(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = ColculateFormuls(1, 2, 3, 4);
+Console.WriteLine(result);
