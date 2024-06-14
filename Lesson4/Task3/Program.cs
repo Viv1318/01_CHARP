7d79ecdf145fcb028448cb85470df1e1bfb70ca0
oleg voronin <oleg83voronin@gmail.com>
2024-06-14 08:16:06 +0300

    add Task3:lesson 4

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать с консоли стороку, состоящую из цифр
+// и латинских букв
+// Сформировать новую строку, состоящую из 
+// букв исходной стороки
+
+string GetLetters (string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+System.Console.WriteLine(str);
